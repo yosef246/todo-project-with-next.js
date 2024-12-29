dca259ad975299f5d04d3969d9c12882b3ad432a
Yosef palas <yosefpalas@gmail.com>
2024-12-29 13:24:11 +0200

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/api/:path*", // שרת ה-API המקומי
+      },
+    ];
+  },
+};
 
 export default nextConfig;
